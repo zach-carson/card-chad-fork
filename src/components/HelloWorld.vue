12565eefd6644e5551dc38c3df3e7f4bdf2479d1
Pandaa <96748487+Pandaalifter@users.noreply.github.com>
2023-02-05 17:50:19 -0800

    Momentous Moment
    Hi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,26 @@
 <template>
   <h1>{{ msg }}</h1>
 
+<!-- My custom code -->
+<button id="duplicate" class="outsideBtn">Duplicate</button>
+<button id="bg" class="outsideBtn">Background</button>
+<button id="header" class="outsideBtn">Heading</button>
+<button id="delete" class="outsideBtn">Delete</button>
+
 <div class="card">
   <h1 class="title">Chad of Cyber IST</h1>
 
   <img class="giaImg" src="https://media.discordapp.net/attachments/963095262363017246/1020131830323744788/unknown.png?width=468&height=468" alt="Professor Giacobe">
   
 <div class="textbox">
-  <input type="checkbox" style='display: none' id=cb checked>
-  <label for="cb" class="haxbtn">Details</label>
-  <div class="description">
-  <p id="content" class="pScale">Professor Giacobe may look like a simple man but perceptions can be misleading. Underneath the facade of a simple college of IST professor lies the holiness of an <strong>IST GOD</strong>!</p>
-  </div>
-  
+  <details>
+    <summary class="haxbtn">Details</summary>
+    <p class="description pScale">Professor Giacobe may look like a simple man but perceptions can be misleading. Underneath the facade of a simple college of IST professor lies the holiness of an <strong>IST GOD</strong>!</p>
+  </details>
+
   </div>
 </div>
+
 
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
